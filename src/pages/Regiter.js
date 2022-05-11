1e96f42972b4e1ed18c60267691eefec8554747e
maliki1234 <malikikowero@gmail.com>
2022-05-11 16:19:47 +0300

    fix some bugs

--- src/pages/Regiter.js
+++ src/pages/Regiter.js
@@ -11,7 +11,7 @@ export default function Login() {
   const onSubmit = data => {
     data = JSON.stringify(data)
     var myHeaders = new Headers();
-    // myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Content-Type", "application/json");
     myHeaders.append('Content-Type', 'application/x-www-form-urlencoded');
 
     myHeaders.append('Access-Control-Allow-Origin', '*');
